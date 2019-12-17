46a8b13c3295eea9220d72e919f385cf5fcf9196
michaeltd <tsouchlarakis@gmail.com>
2019-12-17 03:30:00 +0200

    Some shit.

--- gtkrm.bash
+++ gtkrm.bash
@@ -4,24 +4,21 @@
 
 #shellcheck disable=SC2155 # GO HOME SHELLCHECK, YOU'RE DRUNK!!!
 [[ -z "${GTKDIALOG}" ]] && export GTKDIALOG="$(type -P gtkdialog)"
-
 [[ -z "${GTKDIALOG}" ]] && echo "You need gtkdialog for this!" >&2 && exit 1
 
 source ~/.bashrc.d/.stl/string.sh
-
 #shellcheck disable=SC2155 # GO HOME SHELLCHECK, YOU'RE DRUNK!!!
 export hscalevol="$(digits_only "$(~/bin/sndvol report 2>&1)")"
+
 #shellcheck disable=SC2034 # GO HOME SHELLCHECK, YOU'RE DRUNK!!!
 BR="firefox" ED="gvim" FM="doublecmd" TM="terminology" SH="/bin/tcsh"
-
-export rcfile="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/$(basename "${BASH_SOURCE[0]}").rc"
-
+export rcfile="${HOME}/.$(basename "${BASH_SOURCE[0]}").rc"
 source "${rcfile}"
 
+export todofile="${HOME}/.$(basename "${BASH_SOURCE[0]}").todos"
+
 source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/ABOUT"
-
-source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/OPTIONS"
-
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/OPTS"
 source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/MAIN"
 
 case "${1}" in
