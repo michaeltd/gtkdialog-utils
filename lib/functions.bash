2a6c8289feeaa9b1e6437c6a73bae06ea967e5ff
michaeltd <tsouchlarakis@gmail.com>
2019-12-18 15:44:43 +0200

    For real, this time.

---
+++ lib/functions.bash
@@ -0,0 +1,21 @@
+#source "${HOME}/.bashrc.d/.stl/distance.bash"
+mltokm() {
+    #shellcheck disable=SC2005
+    printf "%.2f\n" "$(echo "scale=2;${1} * 1.609344"|bc -ql)"
+}
+
+kmtoml(){
+    #shellcheck disable=SC2005
+    printf "%.2f\n" "$(echo "scale=2;${1} * 0.621371192237334"|bc -ql)"
+}
+export -f kmtoml mltokm
+
+#source "${HOME}/.bashrc.d/.stl/temp.bash"
+ctof() {
+    echo "scale=2;((9/5) * ${1}) + 32"|bc -ql
+}
+
+ftoc() {
+    echo "scale=2;(${1} - 32) * (5 / 9)"|bc -ql
+}
+export -f ctof ftoc
