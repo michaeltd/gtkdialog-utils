6461d196e5387321e022a1ec34c9063783c5400f
michaeltd <tsouchlarakis@gmail.com>
2019-12-18 10:39:48 +0200

    This is not a commit

--- gtkdutils.bash
+++ gtkdutils.bash
@@ -1,1 +1,20 @@
-bin/gtkdutils.bash
+#!/bin/bash
+#
+#shellcheck source=/dev/null
+
+#shellcheck disable=SC2155 # GO HOME SHELLCHECK, YOU'RE DRUNK!!!
+[[ -z "${GTKDIALOG}" ]] && export GTKDIALOG="$(type -P gtkdialog)"
+[[ -z "${GTKDIALOG}" ]] && echo "You need gtkdialog for this!" >&2 && exit 1
+
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/ui/MAIN"
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/ui/ABOUT"
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/ui/OPTS"
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/ui/CALC"
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/ui/TODOS"
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/ui/FIND"
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/ui/CONVS"
+
+case "${1}" in
+    -d | --dump) echo "${MAIN} ${ABOUT} ${OPTS} ${CALC} ${TODOS} ${FIND} ${CONVS}";;
+    *) "${GTKDIALOG}" --program=MAIN --geometry='+64+64';;
+esac
