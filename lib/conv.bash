8f4af8d51d41a8e15ec63d053431e6fbe59de2ee
michaeltd <tsouchlarakis@gmail.com>
2019-12-23 09:50:33 +0200

    I should have had a V8 this morning.

--- lib/conv.bash
+++ lib/conv.bash
@@ -1,7 +1,8 @@
 #source "${HOME}/.bashrc.d/.stl/distance.bash"
 mltokm() {
     #shellcheck disable=SC2005
-    printf "%.2f\n" "$(echo "scale=2;${1} * 1.609344"|bc -ql)"
+    # printf "%.2f\n" "$(echo "scale=2;${1} * 1.609344"|bc -ql)"
+    printf "%.2f\n" "$(echo "scale=2;${1} / 0.621371192237334"|bc -ql)"
 }
 
 kmtoml(){
@@ -12,20 +13,25 @@ export -f kmtoml mltokm
 
 #source "${HOME}/.bashrc.d/.stl/temp.bash"
 ctof() {
-    echo "scale=2;((9/5) * ${1}) + 32"|bc -ql
+    # printf "%.2f\n" "$(echo "scale=2;((9/5) * ${1}) + 32"|bc -ql)"
+    # °F = °C * 1.8000 + 32.00
+    printf "%.2f\n" "$(echo "scale=2;(${1} * 1.8) + 32"|bc -ql)"
 }
 
 ftoc() {
-    echo "scale=2;(${1} - 32) * (5 / 9)"|bc -ql
+    # printf "%.2f\n" "$(echo "scale=2;(${1} - 32) * (5 / 9)"|bc -ql)"
+    # °C = °F - 32/1.8000
+    printf "%.2f\n" "$(echo "scale=2;(${1} - 32) / 1.8"|bc -ql)"
 }
 export -f ctof ftoc
 
 kgtolb(){
-    # echo "scale=2;${1} * 2.20462262185"|bc -ql
-    echo "scale=2;${1} / 0.45359237"|bc -ql
+    # printf "%.2f\n" "$(echo "scale=2;${1} / 0.45359237"|bc -ql)"
+    printf "%.2f\n" "$(echo "scale=2;${1} * 2.20462262185"|bc -ql)"
 }
 
 lbtokg(){
-    echo "scale=2;${1} * 0.45359237"|bc -ql
+    # printf "%.2f\n" "$(echo "scale=2;${1} * 0.45359237"|bc -ql)"
+    printf "%.2f\n" "$(echo "scale=2;${1} / 2.20462262185"|bc -ql)"
 }
 export -f kgtolb lbtokg
