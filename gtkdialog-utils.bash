832a7bed601eadfecf8ca6b3546195f8330346b2
michaeltd <tsouchlarakis@gmail.com>
2020-03-03 13:00:41 +0200

    yo recipes

--- gtkdialog-utils.bash
+++ gtkdialog-utils.bash
@@ -6,12 +6,18 @@
 [[ -z "${GTKDIALOG}" ]] && export GTKDIALOG="$(type -P gtkdialog)"
 [[ -z "${GTKDIALOG}" ]] && echo "You need gtkdialog for this!" >&2 && exit 1
 
-for i in "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/ui/"*; do
+declare -rx GDU_DIR_NAME="$(dirname "$(realpath "${BASH_SOURCE[0]}")")" \
+	GDU_BASE_NAME="$(basename "$(realpath "${BASH_SOURCE[0]}")")"
+
+declare -rx GDU_RC="${HOME}/.${GDU_BASE_NAME%%\.bash}.rc" \
+	GDU_TODO="${HOME}/.${GDU_BASE_NAME%%\.bash}.todos"
+
+for i in "${GDU_DIR_NAME}/ui/"*; do
     source "${i}"
 done
 
 case "${1}" in
-    h|help|-h|--help) echo "Usage: $(basename "${BASH_SOURCE[0]}") [-(-c)alc]|[-(-s)earch]|[-(-t)odos]|[-o|--conv]" >&2;;
+    h|help|-h|--help) echo "Usage: ${GDU_BASE_NAME} [-(-c)alc]|[-(-s)earch]|[-(-t)odos]|[-o|--conv]" >&2;;
     c|calc|-c|--calc) "${GTKDIALOG}" --program=CALC;;
     s|search|-s|--search) "${GTKDIALOG}" --program=FIND;;
     t|todos|-t|--todos) "${GTKDIALOG}" --program=TODOS;;
--- lib/conv.bash
+++ lib/conv.bash
@@ -1,37 +1,37 @@
 #source "${HOME}/.bashrc.d/.stl/distance.bash"
-mltokm() {
+ml2km() {
     #shellcheck disable=SC2005
     # printf "%.2f\n" "$(echo "scale=2;${1} * 1.609344"|bc -ql)"
     printf "%.2f\n" "$(echo "scale=2;${1} / 0.621371192237334"|bc -ql)"
 }
 
-kmtoml(){
+km2ml(){
     #shellcheck disable=SC2005
     printf "%.2f\n" "$(echo "scale=2;${1} * 0.621371192237334"|bc -ql)"
 }
-export -f kmtoml mltokm
+export -f km2ml ml2km
 
 #source "${HOME}/.bashrc.d/.stl/temp.bash"
-ctof() {
+c2f() {
     # printf "%.2f\n" "$(echo "scale=2;((9/5) * ${1}) + 32"|bc -ql)"
     # °F = °C * 1.8000 + 32.00
     printf "%.2f\n" "$(echo "scale=2;(${1} * 1.8) + 32"|bc -ql)"
 }
 
-ftoc() {
+f2c() {
     # printf "%.2f\n" "$(echo "scale=2;(${1} - 32) * (5 / 9)"|bc -ql)"
     # °C = °F - 32/1.8000
     printf "%.2f\n" "$(echo "scale=2;(${1} - 32) / 1.8"|bc -ql)"
 }
-export -f ctof ftoc
+export -f c2f f2c
 
-kgtolb(){
+kg2lb(){
     # printf "%.2f\n" "$(echo "scale=2;${1} / 0.45359237"|bc -ql)"
     printf "%.2f\n" "$(echo "scale=2;${1} * 2.20462262185"|bc -ql)"
 }
 
-lbtokg(){
+lb2kg(){
     # printf "%.2f\n" "$(echo "scale=2;${1} * 0.45359237"|bc -ql)"
     printf "%.2f\n" "$(echo "scale=2;${1} / 2.20462262185"|bc -ql)"
 }
-export -f kgtolb lbtokg
+export -f kg2lb lb2kg
