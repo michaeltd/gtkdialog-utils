f0b0999f56f988c934ddf707a4a0be4cccc38e33
michaeltd <tsouchlarakis@gmail.com>
2020-04-11 22:51:57 +0300

    Code was clean until manager requested to fuck it up

--- gtkdialog-utils.bash
+++ gtkdialog-utils.bash
@@ -9,10 +9,16 @@
 declare -rx GDU_DIR_NAME="$(dirname "$(realpath "${BASH_SOURCE[0]}")")" \
 	GDU_BASE_NAME="$(basename "$(realpath "${BASH_SOURCE[0]}")")"
 
+declare -rx GDU_UI_DIR="${GDU_DIR_NAME}/ui" \
+	GDU_LIB_DIR="${GDU_DIR_NAME}/lib"
+
 declare -rx GDU_RC="${HOME}/.${GDU_BASE_NAME/\.bash/\.rc}" \
 	GDU_TODO="${HOME}/.${GDU_BASE_NAME/\.bash/\.todos}"
 
-for i in "${GDU_DIR_NAME}/ui/"*\.src; do
+declare -rx TMP_DIR="/tmp/${USER}/$(basename ${BASH_SOURCE[0]/\.bash/})/${$}/"
+mkdir -p "${TMP_DIR}" 
+
+for i in "${GDU_UI_DIR}/"*\.src; do
     source "${i}"
 done
 
