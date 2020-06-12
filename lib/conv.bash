605cd91f41bdd3c549e4ca9ff787236f49b76aa1
michaeltd <tsouchlarakis@gmail.com>
2020-06-12 17:33:51 +0300

    1591972431

--- lib/conv.bash
+++ lib/conv.bash
@@ -27,4 +27,30 @@ lb2kg(){
     #shellcheck disable=SC2005
     printf "%.2f\n" "$(echo "scale=2;${1} / 2.20462262185"|bc -ql)"
 }
-export -f km2ml ml2km c2f f2c kg2lb lb2kg
+
+dec2hex() {
+    echo "obase=16;${1}" | bc -l
+}
+
+dec2bin() {
+    echo "obase=2;${1}" | bc -l
+}
+
+hex2dec() {
+    echo "ibase=16;${1^^}" | bc -l
+}
+
+hex2bin() {
+    echo "obase=2;ibase=16;${1^^}" | bc -l
+}
+
+bin2dec() {
+    echo "ibase=2;${1}" | bc -l
+}
+
+bin2hex() {
+    echo "obase=16;ibase=2;${1}" | bc -l
+}
+
+export -f km2ml ml2km c2f f2c kg2lb lb2kg \
+       dec2hex dec2bin hex2dec hex2bin bin2dec bin2hex
